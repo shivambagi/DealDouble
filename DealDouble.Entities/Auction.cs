41da14eef2b9ef9b6fbda70b39acbd38d2f2b8da
shivambagi <84464752+shivambagi@users.noreply.github.com>
2022-04-04 11:19:39 +0530

    Video 3 - 1) Auction Model 2) Database migration

---
+++ DealDouble.Entities/Auction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DealDouble.Entities
+{
+    public class Auction
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string PictureURL { get; set; }
+        public string Description { get; set; }
+        public decimal ActualAmount { get; set; }
+        public DateTime StartingTime { get; set; }
+        public DateTime EndTime { get; set; }
+
+    }
+}
